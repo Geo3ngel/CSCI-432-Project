1976a2311b782c62eaf5463e64acc1c0b9f86e4d
GeoEngel <GeoEngel.z@gmail.com>
2019-11-24 13:08:12 -0700

    Added initial compression functions to python implementation

--- src/Algorithm.py
+++
@@ -1,1 +0,0 @@
-# This is where any implementations of algorithms will take place for testing purposes/Project demonstraition for the Video.
---
+++ src/BW-transform.py
@@ -0,0 +1,13 @@
+# This is where any implementations of algorithms will take place for testing purposes/Project demonstraition for the Video.
+
+# TODO: Verbos Implentation w/ stepping
+
+# Compresses the string
+def burrows_wheeler_transform(S):
+    pass
+
+# Decompresses the string
+def inverse_burrows_wheeler_transform(L):
+    pass
+
+# TODO: Manual Walkthrough (Task to Tommy?) 
