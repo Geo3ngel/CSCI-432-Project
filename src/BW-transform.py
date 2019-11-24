4d334669a2dafa6337d2e81b84c46273b298deda
GeoEngel <GeoEngel.z@gmail.com>
2019-11-24 13:24:51 -0700

    Finished compression function & added better printing for Matrix M

--- src/BW-transform.py
+++ src/BW-transform.py
@@ -4,10 +4,32 @@
 
 # Compresses the string
 def burrows_wheeler_transform(S):
-    pass
+    L = []
+    M = []
+    # Form all cyclic rotations of S
+    for i in range(0, len(S)):
+        M.append(S[i:]+S[:i])
+        
+    # Sort the rotations Lexicographically
+    M.sort()
+    
+    # Output last column.
+    print_M(M)
+    output = ""
+    for i in range(0, len(M)):
+        output += M[i][-1]
+        
+    return output
 
 # Decompresses the string
 def inverse_burrows_wheeler_transform(L):
     pass
-
+    return S
 # TODO: Manual Walkthrough (Task to Tommy?) 
+
+# Prints out the matrix
+def print_M(M):
+    for row in M:
+        print(row)
+
+print("\nResult:", burrows_wheeler_transform("^BANANA"))
